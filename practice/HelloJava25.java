940333dd350e19f25208884751e95a295f998ae3
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-18 07:45:17 +0300

    Добавлен тестовый файл HelloJava25 и обновлён .gitignore

---
+++ practice/HelloJava25.java
@@ -0,0 +1,6 @@
+public class HelloJava25 {
+     void main() {
+         System.out.println("Hello from Java 25!");
+         System.out.println("JDK version: " + System.getProperty("java.version"));
+    }
+}
