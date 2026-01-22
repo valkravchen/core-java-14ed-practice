b7ffb4ee04d10e8aa9834d0b22f60a9ede8c4f7d
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-22 19:53:17 +0300

    min

--- practice/HelloJava25.java
+++ practice/HelloJava25.java
@@ -2,5 +2,9 @@ public class HelloJava25 {
      void main() {
          System.out.println("Hello from Java 25!");
          System.out.println("JDK version: " + System.getProperty("java.version"));
+         int x = 6;
+         int y = 4;
+         int result = x < y ? x : y;
+         System.out.println(result);
     }
 }
